30e12d7d98318b207eed50e65dc8006734f272c2
rberec <berec@umich.edu>
2017-06-28 02:25:26 -0400

    initial commit, added simple linear regression example

---
+++ src/01_linearRegression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+from  matplotlib import pyplot as plt
+
+from sklearn.datasets import fetch_california_housing
+
+house_data = fetch_california_housing()
+m, n = house_data.data.shape
+
+data = np.c_[np.ones((m, 1)), house_data.data]
+
+X = tf.constant(data, dtype=tf.float32, name="X")
+y = tf.constant(house_data.target.reshape((-1, 1)), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as s:
+    theta_hat = theta.eval()
+    plt.plot(theta_hat)
+    plt.show()
