d4993a9a06ce8b6db4a939847858ec0e9664d799
rberec <berec@umich.edu>
2017-06-29 21:17:45 -0400

    initial commit, added simple linear regression example

--- src/01_linearRegression.py
+++ src/01_linearRegression.py
@@ -3,9 +3,15 @@ import tensorflow as tf
 from  matplotlib import pyplot as plt
 
 from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
 
+# Load data and get the shape
 house_data = fetch_california_housing()
 m, n = house_data.data.shape
+
+# Scale Data
+X_scaler = StandardScaler()
+house_data.data = X_scaler.fit_transform(house_data.data)
 
 data = np.c_[np.ones((m, 1)), house_data.data]
 
@@ -19,3 +25,4 @@ with tf.Session() as s:
     theta_hat = theta.eval()
     plt.plot(theta_hat)
     plt.show()
+    print(theta_hat)
